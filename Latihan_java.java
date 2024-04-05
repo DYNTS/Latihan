aba0f29b03bfd0ae6700c5720ea7d538987ab91f
DYNTS <dianyunita57931@gmail.com>
2024-04-05 20:54:05 +0700

    praktikum 1

---
+++ Latihan_java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihan_java;
+
+/**
+ *
+ * @author DIAN
+ */
+public class Latihan_java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Sepedagunungg.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sepedagunungg;
+
+/**
+ *
+ * @author DIAN
+ */
+public class Sepedagunungg {
+   public static void main(String[] args) {
+       SepedaGunung sepeda = new SepedaGunung();
+       sepeda.tambahKecepatan(10);
+       sepeda.ubahGir(2);
+       sepeda.setSadel(20);
+   }
+}
